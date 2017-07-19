be46f3097be07d17c9fc38323bb1fa90f1d09ce9
Shree Krishna <shreekrishna905@gmail.com>
2017-07-19 17:57:14 +0545

    Register user data convert to json

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -10,18 +10,27 @@ export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
 
+  loading = false;
+
   constructor(private fb: FormBuilder) {
     this.registerForm = fb.group({
       'fullName': ['', Validators.required],
       'email': ['', Validators.compose([Validators.required, Validators.email])],
       'password': ['', Validators.required],
-      'verifyPassword': ['', Validators.required]},
+      'verifyPassword': ['', Validators.required]
+    },
       {
         validator: PasswordValidation.matchPassword('password', 'verifyPassword') // validation for password verification
       });
   }
 
   ngOnInit() {
+  }
+
+  registerUser(registerForm: any): void {
+    const user = this.registerForm.value;
+    const json = {'fullName': user.fullName, 'email': user.email, 'password': user.password, 'enabled': true, 'roles': [{'id': 1, 'name': 'ROLE_USER'}]};
+    console.log(json);
   }
 
 }
